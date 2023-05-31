e19c0d15c81a599cfe676e2bd0c0607ea42f5bc6
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-31 09:18:29 +0530

    updates

--- 09-modularity/app.go
+++ 09-modularity/app.go
@@ -8,7 +8,22 @@ import (
 	"github.com/tkmagesh/cisco-go-may-2023/09-modularity/calculator/utils"
 
 	"github.com/fatih/color"
+	"github.com/tkmagesh/cisco-go-may-2023/09-modularity/models"
 )
+
+/* Attempting to create a method for the Product type in the "models" package */
+// A method in the non-local type
+/*
+func (p models.Product) WhoAmI() string {
+	return "I am a product"
+}
+*/
+
+type MyProduct models.Product
+
+func (p MyProduct) WhoAmI() string {
+	return "I am a product"
+}
 
 func main() {
 	color.Red("app invoked")
@@ -18,4 +33,7 @@ func main() {
 	fmt.Println("Op Count =", calc.OpCount())
 
 	fmt.Println("IsEven(21) =", utils.IsEven(21))
+
+	var p MyProduct
+	fmt.Println(p.WhoAmI())
 }
