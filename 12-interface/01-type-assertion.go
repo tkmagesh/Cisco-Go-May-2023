99c36e3e6f15c1b81c9b72fa1f5ee3a39c84702d
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-31 09:50:24 +0530

    updates

--- 12-interface/01-type-assertion.go
+++ 12-interface/01-type-assertion.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Employee struct {
+	Id int
+}
+
 func main() {
 	// var x interface{}
 	var x any
@@ -27,6 +31,8 @@ func main() {
 	// x = "Occaecat ipsum quis nostrud sunt irure sint laborum nulla in mollit. Dolor aliquip non consectetur eiusmod aute est cillum mollit aliquip cillum. Irure do ullamco consectetur minim enim ipsum labore sint tempor exercitation mollit eiusmod anim qui. Consequat ea ad commodo voluptate. Minim tempor aliqua elit adipisicing id magna aute laborum incididunt ad exercitation magna."
 	// x = true
 	// x = 19.99
+	x = Employee{100}
+	x = struct{}{}
 	switch val := x.(type) {
 	case int:
 		fmt.Println("x is an int, x + 300 =", val+300)
@@ -36,6 +42,10 @@ func main() {
 		fmt.Println("x is a bool, !x =", !val)
 	case float64:
 		fmt.Println("x is a float64, x * 99% =", val*(99.0/100.0))
+	case Employee:
+		fmt.Println("x is an employee with id :", val.Id)
+	case struct{}:
+		fmt.Println("x is an empty object")
 	default:
 		fmt.Println("x is not a known type")
 	}
